8c138886f0e4c530b7130a230c68750759d42937
JaeHee <jaehee1658@gmail.com>
2025-04-09 10:34:28 +0900

    Update Step07CicdApplicationTests.java

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,5 +1,6 @@
 package edu.fisa.ce;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
