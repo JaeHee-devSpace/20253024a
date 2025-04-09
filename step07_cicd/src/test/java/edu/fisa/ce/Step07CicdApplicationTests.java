fd154e672d6db91a29cba6d727631505c11f2b9d
JaeHee <jaehee1658@gmail.com>
2025-04-09 10:49:19 +0900

    Update FailStep07CicdApplicationTests.java

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -10,7 +10,7 @@ class Step07CicdApplicationTests {
 	@Test
 	//@org.junit.jupiter.api.Test // JUnit 5
 	public void testAddition() {
-		int result = 12 + 3;
+		int result = 12 + 13;
 		System.out.println("***비교 전***");
 		assertEquals(15, result); // 성공: 5 == 5
 		System.out.println("***비교 후***");
